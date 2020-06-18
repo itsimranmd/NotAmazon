6281b3619d088d08b963d2d1073dd0a2ccd38d34
yeahthatnerd <itsimranmd@gmail.com>
2020-06-18 16:44:33 +0530

    Published1

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  PORT: process.env.PORT || 5000,
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://testinguser:testinguser@rest-x74aa.mongodb.net/amazon?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
