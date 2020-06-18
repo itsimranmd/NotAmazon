c6ae51984554d673319823868bf24535197beec9
yeahthatnerd <itsimranmd@gmail.com>
2020-06-18 17:23:04 +0530

    Ab

--- backend/server.js
+++ backend/server.js
@@ -30,4 +30,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
-app.listen(config.PORT, () => { console.log('Server started at http://localhost:5000'); });
+app.listen(process.env.PORT || 5000, () => { console.log('Server started at http://localhost:5000'); });
