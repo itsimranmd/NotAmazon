2374026f94975508606cc441c41bb805dae30376
Imran Md <imran@Imrans-MacBook-Pro.local>
2020-08-10 17:44:19 +0530

    publish

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,7 @@ app.get('/api/config/paypal', (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 });
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/build'));
-}
-
+app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
 app.use(express.static(path.join(__dirname, '/../frontend/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
